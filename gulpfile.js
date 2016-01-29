a7f96eeda31fa6b98e89899ca983c6791791a91b
Daniel Eddeland <d.skalle@gmail.com>
2016-01-29 23:23:41 +0100

    Update project structure and add some base css files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,10 @@
 var gulp = require('gulp')
 var babel = require('gulp-babel')
 var jade = require('gulp-jade')
+var sass = require('gulp-sass')
 var webserver = require('gulp-webserver')
 
-gulp.task('default', ['runbabel', 'templates', 'watch', 'webserver'])
+gulp.task('default', ['runbabel', 'templates', 'sass', 'watch', 'webserver'])
 
 gulp.task('runbabel', () => {
   return gulp.src('src/*.js')
@@ -13,9 +14,15 @@ gulp.task('runbabel', () => {
     .pipe(gulp.dest('public'))
 })
 
+gulp.task('sass', () => {
+  gulp.src('src/css/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('public/css'))
+})
+
 gulp.task('templates', () => {
   var myLocals = {}
-  gulp.src('view/*.jade')
+  gulp.src('view/[^_]*.jade')
     .pipe(jade({
       locals: myLocals
     }))
@@ -30,5 +37,6 @@ gulp.task('webserver', () => {
 
 gulp.task('watch', () => {
   gulp.watch('src/*.js', ['runbabel'])
+  gulp.watch('src/css/*.scss', ['sass'])
   gulp.watch('view/*.jade', ['templates'])
 })
--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ console.log('Main js file loaded!')
 console.log('Knockout loaded: ', ko.version)
 
 function AppViewModel() {
-  this.greeting = ko.observable('Hello world!')
+  var self = this;
+  this.greeting = ko.observable('Click an element')
+  this.atoms = [{
+    number: 1,
+    name: 'Hydrogen',
+    letter: 'H'
+  },{
+    number: 2,
+    name: 'Helium',
+    letter: 'He'
+  }]
+  this.atomClicked = function (atom) {
+    self.greeting(atom.name)
+  }
 }
 
 ko.applyBindings(new AppViewModel())
-
