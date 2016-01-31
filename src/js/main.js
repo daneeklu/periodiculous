db123cfe62e0a8d35730b6d04cf6f2170f0101a2
Daniel Eddeland <d.skalle@gmail.com>
2016-01-31 03:30:58 +0100

    Add css transitions to #container

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ console.log('Knockout loaded: ', ko.version)
 
 function AppViewModel() {
   const self = this;
+  let cont = document.getElementById('container')
 
   this.greeting = ko.observable('Click an element')
   this.atoms = ko.observableArray(ptable)
@@ -14,6 +15,23 @@ function AppViewModel() {
   this.atomClicked = function (atom) {
     self.atomList.push(atom)
   }
+
+  this.nextQuestion = () => {
+
+    let resetTransition = function() {
+      let cl = cont.classList;
+      cl.remove('transitioned')
+      cl.add('tLeft')
+      cl.remove('tRight')
+      cont.offsetHeight // flush changes
+      cl.add('transitioned')
+      cl.remove('tLeft')
+      cont.removeEventListener('transitionend', resetTransition)
+    }
+    container.classList.add('tRight')
+    container.addEventListener('transitionend', resetTransition)
+  }
+  document.getElementById('container').classList.remove('tLeft');
 }
 
 ko.applyBindings(new AppViewModel())
